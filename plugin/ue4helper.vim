d24ece6e860963b8865a6704cc11da9d9d7de7e9
nagaohiroki <testplayers0226@gmail.com>
2019-08-15 13:20:41 +0900

    mod

--- plugin/ue4helper.vim
+++ plugin/ue4helper.vim
@@ -4,14 +4,18 @@ endif
 let g:ue4helper=1
 let s:save_cpo = &cpo
 
-function! BuildUE4(ue4_dir)
-	" call setqflist(s:ue4_errorlogs, 'r')
+let s:py_path=expand('<sfile>:h:h') . '/python/ue4helper.py'
+function! s:UE4Dumps()
+	python3 import sys
+	python3 sys.argv = ['-dumps']
+	execute 'py3file ' . s:py_path
+	call setqflist(s:ue4_dumps, 'r')
 	cwindow
 endfunction
-nnoremap <F4> :execute 'term ' . $HOME . '/dotfiles/scripts/ue4_open.bat'<CR>
-nnoremap <F5> :execute 'term ' . $HOME . '/dotfiles/scripts/ue4_build.bat'<CR>
-nnoremap <F6> :execute 'term ' . $HOME . '/dotfiles/scripts/ue4_project.bat'<CR>
-nnoremap <F7> :call BuildUE4('D:/work/UE4/UE_4.22')<CR>
-
+let s:py_cmd = 'term python ' . s:py_path
+command! UE4Build execute s:py_cmd . ' -build'
+command! UE4GenerateProject execute s:py_cmd . ' -generate_project'
+command! UE4OpenProject execute s:py_cmd . ' -open_project'
+command! UE4Dumps call s:UE4Dumps()
 let &cpo = s:save_cpo
 unlet s:save_cpo
--- python/ue4helper.py
+++ python/ue4helper.py
@@ -1,107 +1,128 @@
 import json
 import codecs
 import os
+import sys
 import subprocess
 import re
+is_vim = True
+try:
+    import vim
+except ModuleNotFoundError:
+    is_vim = False
 
 
-class UE4Helper:
-    def build_project(self):
-        param = self.get_param()
-        engine = param['engine_path']
-        project_name = param['project']
-        cmd = [
-            self.get_engine_batch(engine),
-            project_name + 'Editor',
-            'Win64',
-            'Development',
-            '-Project=' + self.get_uproject(param),
-            '-WaitMutex',
-            '-FromMsBuild']
-        subprocess.call(cmd, shell=True)
+def build():
+    param = get_param()
+    engine = param['engine_path']
+    project_name = param['project']
+    cmd = [
+        get_engine_batch(engine),
+        project_name + 'Editor',
+        'Win64',
+        'Development',
+        '-Project=' + get_uproject(param),
+        '-WaitMutex',
+        '-FromMsBuild']
+    subprocess.call(cmd, shell=True)
 
-    def get_errors(self):
-        param = self.get_param()
-        logs = []
-        log = os.path.join(
-            param['engine_path'],
-            'Engine',
-            'Programs',
-            'UnrealBuildTool',
-            'Log.txt')
-        pattern = re.compile(r".*?:   (.*?)\(([0-9]+)\): (.*)")
-        with codecs.open(log, 'r', 'utf-8') as file:
-            for line in file.readlines():
-                if ' error ' in line:
-                    match = re.match(pattern, line)
-                    logs.append({
-                        'filename': match[1].replace('\\', '/'),
-                        'lnum': match[2],
-                        'text': match[3],
-                        'type': 'E'})
-        if not logs:
-            return
-        return str(logs)
 
-    def generate_project(self):
-        param = self.get_param()
-        engine = param['engine_path']
-        project = param['project_path']
-        work_dir = engine
-        generate_project_cmd = 'GenerateProject.bat'
-        if not os.path.exists(os.path.join(engine, generate_project_cmd)):
-            work_dir = project
-            generate_project_cmd = [
-                self.get_engine_batch(engine),
-                '-projectfiles',
-                '-project=' + self.get_uproject(param),
-                '-game',
-                '-rocket']
-        os.chdir(work_dir)
-        subprocess.call(generate_project_cmd, shell=True)
-        subprocess.call(generate_project_cmd + ['-CMakefile'], shell=True)
-        cmake_proc = self.create_compiler_commands(param['vs_common_path'])
-        ctags_proc = self.create_ctags([
-            os.path.join(engine, 'Engine', 'Source'),
-            os.path.join(project, 'Source')])
-        cmake_proc.wait()
-        ctags_proc.wait()
+def dumps():
+    param = get_param()
+    logs = []
+    log = os.path.join(
+        param['engine_path'],
+        'Engine',
+        'Programs',
+        'UnrealBuildTool',
+        'Log.txt')
+    pattern = re.compile(r".*?:   (.*?)\(([0-9]+)\): (.*)")
+    with codecs.open(log, 'r', 'utf-8') as file:
+        for line in file.readlines():
+            if ' error ' in line:
+                match = re.match(pattern, line)
+                logs.append({
+                    'filename': match[1].replace('\\', '/'),
+                    'lnum': match[2],
+                    'text': match[3],
+                    'type': 'E'})
+    if is_vim:
+        vim.command(':let s:ue4_dumps = ' + str(logs))
 
-    def create_compiler_commands(self, vs_common_path):
-        cmd = [
-            os.path.join(vs_common_path, 'Tools', 'VsDevCmd.bat'),
-            '&',
-            'cmake',
-            '-G',
-            'Ninja',
-            '-DCMAKE_EXPORT_COMPILE_COMMANDS=1',
-            '.']
-        return subprocess.Popen(cmd, shell=True)
 
-    def create_ctags(self, source_path):
-        cmd = [
-            'ctags',
-            '--languages=c++',
-            '--output-format=e-ctags']
-        for path in source_path:
-            cmd += ['-R', path]
-        return subprocess.Popen(cmd, shell=True)
+def open_project():
+    subprocess(get_uproject(get_param()), shell=True)
 
-    def get_param(self):
-        return json.load(codecs.open('ue4_project.json', 'r', 'utf-8'))
 
-    def get_engine_batch(self, path):
-        return os.path.join(path, 'Engine', 'Build', 'BatchFiles', 'Build.bat')
+def generate_project():
+    param = get_param()
+    engine = param['engine_path']
+    project = param['project_path']
+    work_dir = engine
+    generate_project_cmd = 'GenerateProject.bat'
+    if not os.path.exists(os.path.join(engine, generate_project_cmd)):
+        work_dir = project
+        generate_project_cmd = [
+            get_engine_batch(engine),
+            '-projectfiles',
+            '-project=' + get_uproject(param),
+            '-game',
+            '-rocket']
+    os.chdir(work_dir)
+    subprocess.call(generate_project_cmd, shell=True)
+    subprocess.call(generate_project_cmd + ['-CMakefile'], shell=True)
+    cmake_proc = create_compiler_commands(param['vs_common_path'])
+    ctags_proc = create_ctags([
+        os.path.join(engine, 'Engine', 'Source'),
+        os.path.join(project, 'Source')])
+    cmake_proc.wait()
+    ctags_proc.wait()
 
-    def get_uproject(self, dic):
-        return os.path.join(dic['project_path'], dic['project'] + '.uproject')
+
+def create_compiler_commands(vs_common_path):
+    cmd = [
+        os.path.join(vs_common_path, 'Common7', 'Tools', 'VsDevCmd'),
+        '&',
+        'cmake',
+        '-G',
+        'Ninja',
+        '-DCMAKE_EXPORT_COMPILE_COMMANDS=1',
+        '.']
+    return subprocess.Popen(cmd, shell=True)
+
+
+def create_ctags(source_path):
+    cmd = [
+        'ctags',
+        '--languages=c++',
+        '--output-format=e-ctags']
+    for path in source_path:
+        cmd += ['-R', path]
+    return subprocess.Popen(cmd, shell=True)
+
+
+def get_param():
+    home = os.path.expanduser('~')
+    path = os.path.join(home, 'ue4_project.json')
+    return json.load(codecs.open(path, 'r', 'utf-8'))
+
+
+def get_engine_batch(path):
+    return os.path.join(path, 'Engine', 'Build', 'BatchFiles', 'Build')
+
+
+def get_uproject(dic):
+    return os.path.join(dic['project_path'], dic['project'] + '.uproject')
 
 
 def main():
-    ue4helper = UE4Helper()
-    # ue4helper.generate_project()
-    # ue4helper.build_project()
-    print(ue4helper.get_errors())
+    if '-build' in sys.argv:
+        build()
+    if '-generate_project' in sys.argv:
+        generate_project()
+    if '-dumps' in sys.argv:
+        dumps()
+    if '-open_project' in sys.argv:
+        open_project()
 
 
 if __name__ == '__main__':
